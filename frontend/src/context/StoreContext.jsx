8ed6878b7e10d5aac3158c6dc7e3546a775413af
chetan mallah <chetanmallah28@gmail.com>
2025-10-11 15:23:01 +0530

    deployment-commit

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -12,8 +12,8 @@ const StoreContextProvider = (props) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);  // Manage authentication status
 const [loading, setLoading] = useState(true);
 
-  // const url = "https://grillz-backend.onrender.com";
-  const url = "http://localhost:4000";
+  const url = "https://book-rental-backend-mjwe.onrender.com";
+  // const url = "http://localhost:4000";
 
   useEffect(() => {
   async function loadData() {
